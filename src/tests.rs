b6561c9abacbbcba9d77321962ac43ee158b84c6
Jean-François Morcillo <jfm@massa.net>
2023-03-28 11:56:53 +0200

    Add tests for wasm implemented in AS
    Signed-off-by: Jean-François Morcillo <jfm@massa.net>

--- src/tests.rs
+++ src/tests.rs
@@ -1,30 +1,57 @@
-mod rust_wasm {
-    use crate::init_runtime;
+use crate::init_runtime;
 
-    #[test]
-    fn test_use_function_from_wasm() {
-        let wasm_bytes = include_bytes!(concat!(
-            env!("CARGO_MANIFEST_DIR"),
-            "/../wasm-module/tmp/wasm_module.wasm"
-        ));
+fn test_add(wasm: &[u8]) {
+    let (mut store, wasm_mod) = init_runtime(wasm);
 
-        let (mut store, wasm_mod) = init_runtime(wasm_bytes);
+    let res = wasm_mod.add(&mut store, 1, 1).unwrap();
+    assert_eq!(res, 2);
 
-        let res = wasm_mod.add(&mut store, 1, 1).unwrap();
-        assert_eq!(res, 2);
+    let res = wasm_mod.add(&mut store, 2, 2).unwrap();
+    assert_eq!(res, 4);
+}
 
-        let res = wasm_mod.add(&mut store, 2, 2).unwrap();
-        assert_eq!(res, 4);
-    }
-    #[test]
-    fn test_use_fn_from_wasm_that_use_native_host_fn() {
-        let wasm_bytes = include_bytes!(concat!(
-            env!("CARGO_MANIFEST_DIR"),
-            "/../wasm-module/tmp/wasm_module.wasm"
-        ));
+fn test_start(wasm: &[u8]) {
+    let (mut store, wasm_mod) = init_runtime(wasm);
+    let res = wasm_mod.start(&mut store).unwrap();
+    assert_eq!(res, 0);
+}
 
-        let (mut store, wasm_mod) = init_runtime(wasm_bytes);
-        let res = wasm_mod.start(&mut store).unwrap();
-        assert_eq!(res, 0);
-    }
+#[test]
+fn test_add_from_rust_wasm() {
+    let wasm_bytes = include_bytes!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/../wasm-module/tmp/wasm_module.wasm"
+    ));
+
+    test_add(wasm_bytes);
+}
+
+#[test]
+fn test_add_from_as_wasm() {
+    let wasm_bytes = include_bytes!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/../wasm-module-as/wasm-module.wasm"
+    ));
+
+    test_add(wasm_bytes);
+}
+
+#[test]
+fn test_start_from_rust_wasm() {
+    let wasm_bytes = include_bytes!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/../wasm-module/tmp/wasm_module.wasm"
+    ));
+
+    test_start(wasm_bytes);
+}
+
+#[test]
+fn test_start_from_as_wasm() {
+    let wasm_bytes = include_bytes!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/../wasm-module-as/wasm-module.wasm"
+    ));
+
+    test_start(wasm_bytes);
 }
