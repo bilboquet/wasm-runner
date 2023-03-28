bilboquet/wasm-module

commit d6fa8b5352958d55213f8e9045a18c919a5ce25f

Call functions with different prototype on host side
Signed-off-by: Jean-François Morcillo <jfm@massa.net>

src/lib.rs
@@ -18,7 +18,11 @@ impl wasm_mod_exported::WasmModExported for WasmModExported {
 
     fn start() -> u32 {
         host_exported::hello();
-        env::abort();
+        env::zero_arg();
+        env::one_arg(1);
+        env::two_arg(2, 1);
+        env::one_string_arg("one string arg");
+        // env::abort("message", 0, 0);
         0u32
     }
 

env.wai
@@ -1,2 +1,7 @@
 // host exported function for wasm modules writen un AS
-abort: func()
+zero-arg: func()
+one-arg: func(a: u32)
+two-arg: func(a: u32, b: u32)
+one-string-arg: func(msg: string)
+//abort2: func( message: option<string>, file-name: option<string>, line-number: u32, column-number: u32)
+abort: func( message: string, line-number: u32, column-number: u32)

bilboquet/wasm-runner

commit eb5a87d51cb804f7107d6685c5546044d143ab47

Fixed memory configuration, add functions with different prototype on host side, all AS tests are broken
works with https://github.com/bilboquet/wasm-module/commit/d6fa8b5352958d55213f8e9045a18c919a5ce25f

Signed-off-by: Jean-François Morcillo <jfm@massa.net>

Cargo.toml
@@ -6,4 +6,5 @@ edition = "2021"
 # See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
 
 [dependencies]
+anyhow = "1.0.70"
 wai-bindgen-wasmer = "0.2.3"

src/env.rs
@@ -1,0 +1,28 @@
+// what this module must provide
+wai_bindgen_wasmer::export!("../wasm-module/tmp/env.wai");
+
+#[derive(Default)]
+pub struct EnvData;
+
+// Implementation block for "services" provided by the host
+impl env::Env for EnvData {
+    fn zero_arg(&mut self) {
+        println!("zero_arg called!");
+    }
+
+    fn one_arg(&mut self, arg: u32) {
+        println!("one_arg called with arg: {}", arg);
+    }
+
+    fn two_arg(&mut self, arg1: u32, arg2: u32) {
+        println!("two_arg called! arg1: {} arg2: {}", arg1, arg2);
+    }
+
+    fn one_string_arg(&mut self, msg: &str) {
+        println!("one_string_arg called with: \"{}\"!", msg);
+    }
+
+    fn abort(&mut self, msg: &str, a: u32, b: u32) {
+        println!("Abort called with: \"{} {} {}\"!", msg, a, b);
+    }
+}

src/lib.rs
@@ -1,10 +1,12 @@
 use crate::host::{host_exported, HostExportedData};
+use env::EnvData;
 use wai_bindgen_wasmer::wasmer::{imports, Module, Store};
 use wasm_mod_exported::WasmModExported;
 
 // what is used from wasm modules
 wai_bindgen_wasmer::import!("../wasm-module/tmp/wasm-mod-exported.wai");
 
+pub mod env;
 pub mod host;
 
 pub fn init_runtime(wasm_bytes: &[u8]) -> (Store, WasmModExported) {
@@ -14,12 +16,21 @@ pub fn init_runtime(wasm_bytes: &[u8]) -> (Store, WasmModExported) {
     let mut import_object = imports! {};
 
     // host env/context ??
-    let data = HostExportedData::default();
+    let host_data = HostExportedData::default();
     // provide native functions to wasm module
-    let _f = host_exported::add_to_imports(&mut store, &mut import_object, data);
+    let apply_config_for_host = host_exported::add_to_imports(&mut store, &mut import_object, host_data);
 
-    let (wasm_mod, _instance) =
+    let env_data = EnvData::default();
+    // provide native functions to wasm module
+    let apply_config_for_env = env::env::add_to_imports(&mut store, &mut import_object, env_data);
+
+    let (wasm_mod, instance) =
         WasmModExported::instantiate(&mut store, &module, &mut import_object).unwrap();
+
+    // MUST be called, at least for the memory to be shared
+    apply_config_for_host(&instance, &store).unwrap();
+    apply_config_for_env(&instance, &store).unwrap();
+
     (store, wasm_mod)
 }
 

src/tests.rs
@@ -16,6 +16,14 @@ fn test_start(wasm: &[u8]) {
     assert_eq!(res, 0);
 }
 
+fn test_greet(wasm: &[u8]) {
+    let (mut store, wasm_mod) = init_runtime(wasm);
+
+    let res = wasm_mod.greet(&mut store, "Paul").unwrap();
+
+    assert_eq!(res, "Hello Paul!".to_string());
+}
+
 #[test]
 fn test_add_from_rust_wasm() {
     let wasm_bytes = include_bytes!(concat!(
@@ -54,4 +62,24 @@ fn test_start_from_as_wasm() {
     ));
 
     test_start(wasm_bytes);
+}
+
+#[test]
+fn test_greet_from_rust_wasm() {
+    let wasm_bytes = include_bytes!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/../wasm-module/tmp/wasm_module.wasm"
+    ));
+
+    test_greet(wasm_bytes);
+}
+
+#[test]
+fn test_greet_from_as_wasm() {
+    let wasm_bytes = include_bytes!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/../wasm-module-as/wasm-module.wasm"
+    ));
+
+    test_greet(wasm_bytes);
 }